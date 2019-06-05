139e52b62c39af1f7664697cd7836b0a0dbca559
Terry Threatt <terry.threatt@gmail.com>
2019-06-05 13:59:26 -0500

    added yargs and commands

--- app.js
+++ app.js
@@ -1,10 +1,47 @@
 const getNotes = require('./notes');
 const chalk = require('chalk');
-const log = console.log;
+const yargs = require('yargs');
 
-const msg = getNotes()
-console.log(msg)
+// Customize yargs version 
+yargs.version('1.1.0')
 
-log(chalk.green.bold.underline('Success'));
+// Create add command 
+yargs.command({ 
+    command: 'add', 
+    describe: 'Add a new note', 
+    handler: function () { 
+        console.log('Adding a new note')
+    }
+})
+
+// Create a remove command 
+yargs.command({ 
+    command: 'remove', 
+    describe: 'Removing a note',
+    handler: function () { 
+        console.log('Removing the note')
+    }
+})
+
+// Read a command 
+yargs.command({ 
+    command: 'read', 
+    describe: 'Reading a note',
+    handler: function () {
+        console.log("Reading the note")
+    }
+})
+
+// Listing a command 
+yargs.command({
+    command: 'list', 
+    describe: 'List your notes', 
+    handler: function () { 
+        console.log('Listing the notes')
+    }
+})
+// add, remove, read, list 
+
+console.log(yargs.argv)
 
 
